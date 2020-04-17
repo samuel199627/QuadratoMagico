abdeca9b7b99f101215b79bafaf95f8b9910297f
samuel <samuel@localhost>
2020-04-17 11:45:55 +0200

    aggiunta miei commenti alla soluzione caricata dal professore

--- src/main/java/it/polito/tdp/quadrato/model/RisolviQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/RisolviQuadrato.java
@@ -10,17 +10,24 @@ public class RisolviQuadrato {
 	
 	private List<List<Integer>> soluzioni;
 	
+	//nel costruttore inializziamo la dimensione del lato del quadrato
+	//e ci salviamo anche per comodita' il numero di caselle siccome
+	//e' una quantita' che compare spesso nel codice
 	public RisolviQuadrato(int N) {
 		this.N = N ;
 		this.N2 = N*N ;
+		//inizializziamo anche la somma corretta che sappiamo che ci deve
+		//essere una volta nota la dimensione della riga e della colonna
 		this.magica = N*(N2+1)/2 ;
 	}
 	
 	// Calcola tutti i quadrati magici
 	public List<List<Integer>> quadrati() {
+		//inizializziamo la ricorsione
 		List<Integer> parziale = new ArrayList<>() ;
 		int livello = 0 ;
 		
+		//ogni soluzione e' una lista di interi
 		this.soluzioni = new ArrayList<List<Integer>>() ;
 		
 		cerca(parziale, livello) ;
@@ -31,39 +38,76 @@ public class RisolviQuadrato {
 	// procedura ricorsiva (privata)
 	private void cerca(List<Integer> parziale, int livello) {
 		
+		//se sono all'ultima casella allora ho un quadrato completo
 		if(livello==N2) {
 			// caso terminale
+			//controllo il mio quadrato se e' magico oppure no
 			if(controlla(parziale)) {
 				// è magico!!
-				System.out.println(parziale) ;
+				//System.out.println(parziale) ;
+				//aggiungo alla soluzione solo se e' un quadrato magico
+				//occhio che devo crearmi un nuovo oggetto inizializzandolo
+				//con il parziale e non passare il parziale direttamente altrimenti
+				//passiamo il riferimento all'oggetto e poiche' parziale viene 
+				//mutato dalla ricorsione, se erroneamente passiamo il riferimento 
+				//all'oggetto nella soluzione, allora tutti gli elementi nel vettore
+				//soluzione punteranno allo stesso oggetto parziale e conterranno tutti
+				//la stessa cosa.
+				//quindi importante passare un nuovo oggetto caricato con gli
+				//oggetti che popolano parziale che sono interi e quindi immutabili
 				this.soluzioni.add(new ArrayList<>(parziale)) ;
 			}
+			//arrivato al livello massimo comunque devo tornare indietro
+			//con la ricorsione, sia che io sia con un quadrato magico 
+			//oppure no
 			return ;
 		}
 		
 		// controlli intermedi, quando livello è multiplo di N (righe complete)
+		//controllo il resto della divisione di livello per N.
+		//Senza questo controllo, gia' con il lato di dimensione 4 il programma non
+		//termina in quanto il fattoriale di 16 e' un numero enorme.
+		//Con questo controllo tagliamo gia' notevolmente molti rami che non
+		//portavano da nessuna parte
 		if(livello%N==0 && livello!=0) {
+			//controlliamo che l'ultima riga inserita sommi a quello giusto a cui
+			//noi sappiamo che deve sommare.
+			//Se la riga non somma correttamente ritorno indietro con la ricorsione di
+			//un livello e quindi completo la riga con un numero diverso in quanto
+			//tolgo l'ultimo numero che avevo messo e se ce ne sono ancora da provare
+			//lo rimpiazza con un'altra
 			if(!controllaRiga(parziale, livello/N-1))
 				return ; // potatura (pruning) dell'albero di ricerca
 		}
 		
-		// caso intermedio
+		// caso intermedio in cui scorro tutti i numeri che 
+		//possono popolare il quadrato e se non sono ancora nel quadrato
+		//parziale allora la vado a mettere
 		for(int valore=1; valore<=N2; valore++) {
 			if(!parziale.contains(valore)) {
 				// prova 'valore'
 				parziale.add(valore) ;
 				cerca(parziale, livello+1);
+				//tolgo l'ultimo numero che avevo inserito a questo livello
+				//per poterne valutare altri in questo livello di inserimento
+				//nel quadrato
 				parziale.remove(parziale.size()-1) ;
 			}
 		}
 	}
 		
+	
+	//questa funzione prende una soluzione, che deve riempire il quadrato in totale
+	//e controlla se e' un quadrato magico oppure no
 	/**
 	 * Verifica se una soluzione rispetta tutte le somme
 	 * @param parziale
 	 * @return
 	 */
 	private boolean controlla(List<Integer> parziale) {
+		
+		//e' solo una funzione molto noiosa in cui dobbiamo controllare linee, diagonali
+		//e colonne che sommino uguali
 		if(parziale.size()!=this.N*this.N)
 			throw new IllegalArgumentException("Numero di elementi insufficiente") ;
 		
@@ -105,6 +149,7 @@ public class RisolviQuadrato {
 
 		return true ;
 	}
+	
 	
 	private boolean controllaRiga(List<Integer> parziale, int riga) {
 		int somma=0;
--- src/main/java/it/polito/tdp/quadrato/model/TestQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/TestQuadrato.java
@@ -1,14 +1,30 @@
 package it.polito.tdp.quadrato.model;
 
+//c'e' il pdf in cui spiega tutto quello che riguarda il problema
+
+//funziona massimo con il quadrato massimo li lato 4
+//gia' con il lato 5 il programma non termina in quanto anche con il taglio
+//di rami non e' cosi' sufficiente
+
 import java.util.List;
 
 public class TestQuadrato {
 
 	public static void main(String[] args) {
 
-		RisolviQuadrato r = new RisolviQuadrato(4) ;
+		System.out.println("LATO TRE: \n\n");
+		RisolviQuadrato r = new RisolviQuadrato(3) ;
 		
 		List<List<Integer>> soluzioni = r.quadrati();
+		
+		for(List<Integer> sol: soluzioni) {
+			System.out.println(sol);
+		}
+		
+		System.out.println("LATO QUATTRO: \n\n");
+		RisolviQuadrato r2 = new RisolviQuadrato(4) ;
+		
+		List<List<Integer>> soluzioni2 = r2.quadrati();
 		
 		for(List<Integer> sol: soluzioni) {
 			System.out.println(sol);
